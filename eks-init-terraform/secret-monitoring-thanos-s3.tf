c9d5c38235a316c2eab24a89bfc1fb47a68347d6
fresh-tuna <freshtuna@kakao.com>
2024-03-12 14:26:25 +0900

    bug fix

--- eks-init-terraform/secret-monitoring-thanos-s3.tf
+++ eks-init-terraform/secret-monitoring-thanos-s3.tf
@@ -5,7 +5,7 @@ resource "kubernetes_secret" "secret-monitoring-thanos-s3" {
   }
 
   data = {
-    "config" = "${file("${path.module}/.credentials/secret-monitoring-thanos-s3.yaml")}"
+    "config" = "${file("${path.module}/secret-monitoring-thanos-s3.yaml")}"
   }
 
 #   type = "kubernetes.io/dockerconfigjson"
